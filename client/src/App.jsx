1b549e2b0fb6cabbed983e49b61989ad707f64d1
Rahul Prajapati <rahul.prajapati1127@gmail.com>
2024-02-28 15:09:12 +0530

    Added server and database, model and pages

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Header from '../src/components/Header'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Home from './pages/Home'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/profile' element={<Profile />} />
+    </Routes>
+  </BrowserRouter>
+    
+  )
+}
+
+export default App
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+const Header = () =>{
+    return (
+       <div className='flex justify-between bg-slate-200 m-3 p-4' >
+        <h1>
+            <span className='font-bold text-3xl text-red-500 ' >Empire Estate</span>
+        </h1>
+        <form className='flex bg-slate-100 rounded-xl p-1'>
+            <input className='bg-transparent py-1 rounded-lg focus:outline-none w-24 sm:w-64' type='text' placeholder=' Search' />
+            <FaSearch className=' my-3  ' />
+            
+        </form>
+        <ul className='flex p-1 gap-1'>
+            <li className='sm:inline hover:underline '><Link to="/">Home</Link></li>
+            <li className='sm:inline hover:underline '><Link to="/about">About</Link></li>
+            <li className='sm:inline hover:underline '><Link to="/profile">Profile</Link></li>
+            <li className='sm:inline hover:underline '><Link to="/login">Login</Link></li>
+        </ul>
+        
+       </div>
+    )
+}
+
+export default Header
